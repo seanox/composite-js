201e5b6a3c05bc31fa5d45f12b02256a68e60b94
seanox <service@seanox.com>
2026-07-28 20:36:25 +0200

    #0000 Tutorials: Update of content

--- tutorials/micro-frontend/Step_02 Integration of Seanox aspect-js/assets/aspect-js-max.js
+++ tutorials/micro-frontend/Step_02 Integration of Seanox aspect-js/assets/aspect-js-max.js
@@ -1581,12 +1581,12 @@
 /**
  * With aspect-js the declarative approach of HTML is taken up and extended.
  * In addition to the expression language, the HTML elements are provided with
- * additional attributes for functions and view model binding. The corresponding
+ * additional attributes for functions and view-module binding. The corresponding
  * renderer is included in the composite implementation and actively monitors
  * the DOM via the MutationObserver and thus reacts recursively to changes in
  * the DOM.
  *
- * This is the static component for rendering and the view model binding.
+ * This is the static component for rendering and the view-module binding.
  * Processing runs in the background and starts automatically when the page is
  * loaded.
  *
@@ -1616,7 +1616,7 @@
  * themselves, and be a link between the user interface (view) and middleware
  * (backend).
  *
- * The required view model binding is part of the Model View Controller and the
+ * The required view-module binding is part of the Model View Controller and the
  * Composite API.
  *
  *         property
@@ -1631,7 +1631,7 @@
  * In some cases, the identifier (ID) may not be unique. For example, in cases
  * where properties are arrays or an iteration is used. In these cases the
  * identifier can be extended by an additional unique qualifier separated by a
- * colon. Qualifiers behave like properties during view model binding and extend
+ * colon. Qualifiers behave like properties during view-module binding and extend
  * the namespace.
  *
  *         unique
@@ -2339,7 +2339,7 @@
 
 		/**
 		 * Mounts the as selector passed element(s) with all its children where
-		 * a view model binding is possible. Mount is possible for all elements
+		 * a view-module binding is possible. Mount is possible for all elements
 		 * with ATTRIBUTE_ID, not only for composite objects and their children.
 		 *
 		 * View-model binding is about linking of HTML elements in markup (view)
@@ -2352,7 +2352,7 @@
 		 * logic themselves, and be a link between the user interface (view) and
 		 * middleware (backend).
 		 *
-		 * The required view model binding is part of the Model View Controller
+		 * The required view-module binding is part of the Model View Controller
 		 * and the Composite API.
 		 *
 		 * The view as presentation and user interface for interactions and the
@@ -3414,7 +3414,7 @@
 
 				// The attributes ATTRIBUTE_EVENTS, ATTRIBUTE_VALIDATE and
 				// ATTRIBUTE_RENDER are processed in Composite.mount(selector)
-				// the view model binding and are only mentioned here for
+				// the view-module binding and are only mentioned here for
 				// completeness.
 
 				// The attribute ATTRIBUTE_RELEASE has no functional
@@ -4540,7 +4540,7 @@
 				});
 
 				// All removed elements are cleaned and if necessary the undock
-				// method is called if a view model binding exists.
+				// method is called if a view-module binding exists.
 				(record.removedNodes || []).forEach((node) =>
 					_cleanup(node));
 			});
@@ -4997,7 +4997,7 @@
  *         ----
  * Paths are used for navigation, routing and controlling the view flow. The
  * target can be a view or a function if using interceptors. For SPAs
- * (Single-Page Applications), the anchor part of the URL is used for navigation
+ * (single-page applications), the anchor part of the URL is used for navigation
  * and routes.
  *
  * Similar to a file system, absolute and relative paths are also supported

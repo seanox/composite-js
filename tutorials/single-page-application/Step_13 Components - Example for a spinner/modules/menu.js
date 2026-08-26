9d6b3537e4195dfe4d74fb8d593a86c70bbc0a89
seanox <service@seanox.com>
2026-08-26 07:47:50 +0200

    #0000 Tutorials: Update of content

--- tutorials/single-page-application/Step_13 Components - Example for a spinner/modules/menu.js
+++ tutorials/single-page-application/Step_13 Components - Example for a spinner/modules/menu.js
@@ -1,8 +1,8 @@
-// Modules (Composite-JavaScript) use their own scope. Variables and constants
-// created and used here are not accessible outside and must be exported for use
-// in the global scope, for which the macro #export is used.
+// Composite scripts are executed in an isolated module scope. Variables and
+// constants created and used here are not accessible outside and must be
+// exported for use in the global scope, for which the macro #export is used.
 //
-// The JavaScript models correspond to the DOM hierarchy and are based on the
+// The application module corresponds to the DOM hierarchy and is based on the
 // declared ID attributes: #menu -> #language -> event(s)
 const menu = {
     language: {

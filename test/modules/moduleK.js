55129d0a7a20442c90afdb5354a35dc0d428775f
seanox <service@seanox.com>
2026-09-02 22:00:46 +0200

    #0000 Test: Update of test cases

--- test/modules/moduleK.js
+++ test/modules/moduleK.js
@@ -1,4 +1,6 @@
-// because of "use strict" this will be an error
+// The module is executed in a separate function scope.
+// Without an explicit #export, moduleE is not added to the global scope and is
+// therefore not accessible from outside this module.
 moduleE = {
     test: "E"
 };
